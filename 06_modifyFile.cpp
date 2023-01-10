bdf469b7b95b86d51b75e9fdbda851975701eb10
Rolo27s <108833691+Rolo27s@users.noreply.github.com>
2023-01-10 16:49:38 +0100

    Add files via upload

---
+++ 06_modifyFile.cpp
@@ -0,0 +1,42 @@
+/* ENCODING USED: windows-1252 to solve special characters*/
+#include <iostream>
+#include <stdlib.h>
+#include <locale.h>
+#include <wchar.h>
+#include <fstream>
+
+using namespace std;
+
+void modificar();
+
+int main(){
+    setlocale(LC_ALL, "");
+    
+    modificar();
+}
+
+void modificar(){
+    
+    // ofstream: escribir en un fichero             + archivo.open ios::out
+    // ifstream: abrir un fichero de lectura        + archivo.open ios::in
+    // fstream: añadir datos al final del fichero   + archivo.open ios::app (append)
+
+
+    fstream archivo;
+    string nombreArchivo, texto;
+
+    cout << ("Introduce el nombre del archivo de texto a buscar para modificarlo (la extension del archivo de texto se añade automáticamente): ");
+    getline(cin >> std::ws,nombreArchivo);
+    nombreArchivo += ".txt";
+
+    archivo.open(nombreArchivo.c_str(), ios::app);
+
+    if(archivo.fail()){
+        cout << "No se encontro el archivo" << endl;
+    }else {
+        archivo << endl << "Frase nueva generada por la funcion 'fstream', quedando modificado..." << endl;
+    }
+
+    cout << endl;
+    archivo.close();
+}
